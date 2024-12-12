9bf4db190e3d3729fe81937e01a2e9c6ad804f7d
DostonbekZaripov <dastandev0110@gmail.com>
2024-12-13 02:26:03 +0500

    The end

---
+++ main.js
@@ -0,0 +1,13 @@
+// class Person {
+//     constructor(name,age,){
+//         this.name= name,
+//         this.age=age
+//     }
+//   john(){
+//     return `${this.name} age is ${this.age}`
+//   }
+// }
+// const person = new Person ("John",34)
+// const person2 = person.john()
+// console.log(person2);
+
